186bd4abd3960dea16aed0c94984650c244cf69e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-09 10:13:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -7,13 +7,16 @@ def my_each(array) # put argument(s) here
     yield array[i]
     i = i + 1
   end
-  
-  
-    array
-else
-  puts "Hey! No block was given!"
-  end
 end
+end
+end
+  
+  
+    # array
+# else
+#   puts "Hey! No block was given!"
+#   end
+# end
 
 
 
