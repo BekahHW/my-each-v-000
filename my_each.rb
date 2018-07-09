7bb2a338350d6254628122d12a5eff25ae29ce7a
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-09 10:23:41 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,20 +1,10 @@
-def my_each(array) # put argument(s) here
-  # if block_given?
-    i = 0
- 
+def my_each(array)
+  i = 0
   while i < array.length
     yield(array[i])
-    i = i + 1
-  # end
-# end
-end
-# end
-  
-  
-    array
-# else
-#   puts "Hey! No block was given!"
-  # end
+    i = i +1
+  end
+  array
 end
 
 
@@ -29,8 +19,3 @@ end
 
 
 
-
-# collection = [1, 2, 3, 4]
-# my_each(collection) do |i|
-#   puts i
-# end
